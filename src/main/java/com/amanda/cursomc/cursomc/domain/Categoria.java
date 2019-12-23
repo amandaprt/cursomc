c454cf114dc1d085650d062e743ff0b950db72b0
Amanda Poreto <amandaporeto@hotmail.com>
2019-12-23 17:08:18 -0300

    Criando dominio categoria

---
+++ src/main/java/com/amanda/cursomc/cursomc/domain/Categoria.java
@@ -0,0 +1,36 @@
+package com.amanda.cursomc.cursomc.domain;
+
+public class Categoria {
+
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/main/java/com/amanda/cursomc/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/amanda/cursomc/cursomc/resource/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.amanda.cursomc.cursomc.resource;
 
+import com.amanda.cursomc.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String teste(){
-        return "TESTANDO SE ESTÁ FUNCIONANDO";
+    public List<Categoria> teste(){
+        Categoria categoria = new Categoria(1, "Caneta");
+        Categoria categoria1 = new Categoria(2, "lápis");
+
+        List<Categoria> listaCategoria = new ArrayList<>();
+        listaCategoria.add(categoria);
+        listaCategoria.add(categoria1);
+
+        return listaCategoria;
+
+
     }
 }
